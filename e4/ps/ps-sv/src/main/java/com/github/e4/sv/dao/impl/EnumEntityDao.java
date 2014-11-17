95837ad9d319efa6767b384134b02947670b8daf
心道 <howard.hb@alibaba-inc.com>
2014-11-17 10:44:43 +0800

    添加Jmock和Test4j的依赖 修改Junit的版本为4

--- e4/ps/ps-sv/src/main/java/com/github/e4/sv/dao/impl/EnumEntityDao.java
+++ e4/ps/ps-sv/src/main/java/com/github/e4/sv/dao/impl/EnumEntityDao.java
@@ -1,25 +1,11 @@
 package com.github.e4.sv.dao.impl;
 
-import java.util.List;
-
-import org.hibernate.criterion.DetachedCriteria;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.stereotype.Repository;
-
 import com.github.e4.da.hibernate.HibernateBaseDao;
 import com.github.e4.sv.entity.EnumEntity;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class EnumEntityDao extends HibernateBaseDao<EnumEntity, String>{
 
-	public EnumEntity findByCode(String code) {
-		DetachedCriteria dc = createDetachedCriteria();
-		dc.add(Restrictions.eq("code", code));
-		List<EnumEntity> list = query(dc);
-		if(list == null || list.size() == 0){
-			return null;
-		}
-		return list.get(1);
-	}
 
 }
---
+++ e4/ps/ps-sv/src/test/java/com/github/e4/sv/service/GlobalVariableServiceTest.java
@@ -0,0 +1,15 @@
+package com.github.e4.sv.service;
+
+import com.github.e4.sv.service.impl.GlobalVariableService;
+
+/**
+ * Created by howard.hb on 2014/11/16.
+ */
+public class GlobalVariableServiceTest {
+
+    private GlobalVariableService globalVariableService;
+
+    public void createTest(){
+
+    }
+}
